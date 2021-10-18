99b5459d356e3359d51f1d4321d7167761c41e82
Charlie Cantrell <ccantr19@utk.edu>
2021-10-18 15:03:32 -0400

    back to basics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,11 @@ export default function Page() {
   const { useQuery } = client;
   const generalSettings = useQuery().generalSettings;
   
-  const allPages = await getArrayFields(useQuery().pages().nodes, 'isFrontPage', 'id');
+  const allPages = getArrayFields(useQuery().pages().nodes, 'isFrontPage', 'id');
   //const cleanAllPages = castNotSkeleton({ allPages });
-  const frontPage = allPages.find(x => x.isFrontPage === true);
+  //const frontPage = allPages.find(x => x.isFrontPage === true);
   
-  console.log(frontPage);
+  console.log(allPages);
 
   return (
     <>
