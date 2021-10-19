537a82cb47388c9c73bcd3bacfb81b86851d36c5
Charlie Cantrell <ccantr19@utk.edu>
2021-10-19 16:23:25 -0400

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Page() {
   const allPages = getArrayFields(useQuery().pages().nodes, 'isFrontPage', 'id');
   //const frontPage = allPages.find(x => x.isFrontPage === true);
   
-  console.log(allPages.find(x => x.isFrontPage === true).id);
+  console.log(allPages.findIndex(x => x.isFrontPage === true));
 
   return (
     <>
