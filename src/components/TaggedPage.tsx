1afcb1979ba264945dada20034ec374b14e3d407
Charlie Cantrell <ccantr19@utk.edu>
2021-11-09 13:12:40 -0500

    testing tags

--- src/components/TaggedPage.tsx
+++ src/components/TaggedPage.tsx
@@ -10,8 +10,8 @@ interface TaggedPageProps {
 export default function TaggedPage({ id, contentType, typeName }: TaggedPageProps): JSX.Element {
     const { useQuery } = client;
 
-    //console.log(id);
-    //console.log(contentType);
+    console.log(id);
+    console.log(contentType);
     
 
     if(contentType === 'TimelineEvent'){
@@ -23,13 +23,17 @@ export default function TaggedPage({ id, contentType, typeName }: TaggedPageProp
     //const thisQuery = useQuery()[contentCall];
 
    
-    var thisPage = useQuery()[contentCall]({id: id});
+    //var thisPage = useQuery()[contentCall]({id: id});
 
-    //console.log(contentCall);
+    //this works
+    console.log(useQuery()[contentCall]);
+
+    //but this throws a compile error
+    //console.log(useQuery()[contentCall]({id: id}));
 
   return (
     <div>
-      <h3>{thisPage.title()
+      <h3>{//thisPage.title()
       }
       </h3>
     </div>
