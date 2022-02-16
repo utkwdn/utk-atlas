4a58bcfd2a543c703f5725344af11e57cbf642f8
spworky <69162526+spworky@users.noreply.github.com>
2022-02-16 15:26:25 -0500

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import 'faust.config';
-import { FaustProvider } from '@faustjs/next';
-import 'normalize.css/normalize.css';
-import React from 'react';
-import 'scss/main.scss';
-import { client } from 'client';
-import type { AppProps } from 'next/app';
+import "faust.config";
+import { FaustProvider } from "@faustjs/next";
+import "normalize.css/normalize.css";
+import React from "react";
+import "scss/main.scss";
+import { client } from "client";
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
