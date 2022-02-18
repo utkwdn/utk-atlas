2476a9d357a7ad7b9f574e7e6eb76d2518f027f5
Charlie Cantrell <ccantr19@utk.edu>
2022-02-18 17:39:37 -0500

    Adding A to Z index. Won't sort yet.

---
+++ src/pages/alpha.tsx
@@ -0,0 +1,82 @@
+import { getNextStaticProps } from '@faustjs/next';
+import { client, OrderEnum, PostObjectsConnectionOrderbyEnum } from 'client';
+import { Footer, Header, Hero } from 'components';
+import { GetStaticPropsContext } from 'next';
+import Head from 'next/head';
+
+export default function Page() {
+  const { useQuery } = client;
+  const generalSettings = useQuery().generalSettings;
+
+    const alphaIndex = useQuery().allAToZ({
+      first: 10000,
+      where:{
+        orderby:{
+          field: PostObjectsConnectionOrderbyEnum.TITLE,
+          order: OrderEnum.ASC
+        }
+      }
+    });
+
+   console.log(alphaIndex.nodes[0].aToZFields.url);
+
+  return (
+    <>
+      <Header
+        title={generalSettings.title}
+        description={generalSettings.description}
+      />
+
+      <Head>
+        <title>A-Z Index - {generalSettings.title}</title>
+      </Head>
+
+      <Hero title="A-Z Index" />
+
+      <main className="content content-single">
+        <div className="wrap">
+         
+
+        <div className='table-responsive'>
+          <table className='table table-striped'>
+            <thead>
+              <tr>
+                <th>Site</th>
+                <th>Category</th>
+              </tr>
+            </thead>
+            <tbody>
+            {alphaIndex.nodes.map((this_alpha) => (
+              <tr>
+                <td><a href={this_alpha.aToZFields.url}>
+                  <strong>{this_alpha.title()}</strong>
+                  <br />
+                  <small className='utsmokey alpha-link'>{this_alpha.aToZFields.url}</small>
+                </a></td>
+                <td className='alph-cat'>
+                  <small>
+                    {this_alpha.aToZCategories().nodes.map((this_cat) => (
+                      <a className='utsmokey'>{this_cat.name}</a> 
+                    ))}
+                  </small>
+                </td>
+              </tr>
+            ) ) }
+            </tbody>
+          </table>
+        </div>
+
+        </div>
+      </main>
+
+      <Footer copyrightHolder={generalSettings.title} />
+    </>
+  );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return getNextStaticProps(context, {
+    Page,
+    client,
+  });
+}
