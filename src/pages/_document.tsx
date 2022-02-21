055678eb3982521c6bc505ad2956690a912e0ac4
Purdy, Michael Shawn <purdy@utk.edu>
2022-02-21 13:51:35 -0500

    Adding gotham to the site.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,29 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="icon" href="/images/favicons/favicon.ico" sizes="any" />
-        <link rel="icon" href="/images/favicons/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/images/favicons/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-
+        <link
+          rel="icon"
+          href="/images/favicons/favicon.ico"
+          sizes="any"
+        />
+        <link
+          rel="icon"
+          href="/images/favicons/icon.svg"
+          type="image/svg+xml"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/images/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="manifest"
+          href="/manifest.json"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cloud.typography.com/6831932/6125612/css/fonts.css"
+        />
         </Head>
         <body>
           <Main />
