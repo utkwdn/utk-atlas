ad5bc05f4045a9f622d573edff60ddd54b7fe4a3
Purdy, Michael Shawn <purdy@utk.edu>
2022-04-22 15:11:54 -0400

    Putting local links in the header so they'll work in all environments.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,10 +26,10 @@ function Header({
     <nav id="universal-header">
         <div className="container-xxl">
         <ul className="row justify-content-end list-inline mb-0">
-          <li className="col-auto"><small><a href="https://www.utk.edu/requestinfo/" className="nav-link px-0 text-light text-uppercase">Request Info</a></small></li>
-          <li className="col-auto"><small><a href="https://www.utk.edu/visit/" className="nav-link px-0 text-light text-uppercase">Visit</a></small></li>
-          <li className="col-auto"><small><a href="https://www.utk.edu/apply/" className="nav-link px-0 text-light text-uppercase">Apply</a></small></li>
-          <li className="col-auto"><small><a href="https://www.utk.edu/give/" className="nav-link px-0 text-light text-uppercase">Give</a></small></li>
+          <li className="col-auto"><small><a href="/requestinfo/" className="nav-link px-0 text-light text-uppercase">Request Info</a></small></li>
+          <li className="col-auto"><small><a href="/visit/" className="nav-link px-0 text-light text-uppercase">Visit</a></small></li>
+          <li className="col-auto"><small><a href="/apply/" className="nav-link px-0 text-light text-uppercase">Apply</a></small></li>
+          <li className="col-auto"><small><a href="/give/" className="nav-link px-0 text-light text-uppercase">Give</a></small></li>
          <li className="col-auto"> <button type="button" id="btn-searchopen" className="btn btn-search text-uppercase text-light navbar-toggler col-auto collapsed px-0" data-bs-toggle="modal" data-bs-target="#searchModal" aria-label="Toggle search"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24" id="searchHeader-open"><path d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z"></path></svg>
          <span>Search</span></button></li>
         </ul>
@@ -136,10 +136,10 @@ function Header({
                 <nav id="universal-header">
                   <div className="container">
                     <ul className="row justify-content-end list-inline mb-0">
-                      <li className="col-auto"><small><a href="https://www.utk.edu/requestinfo/" className="nav-link px-0 text-light text-uppercase text-small text-white btn-request">Request Info</a></small></li>
-                      <li className="col-auto"><small><a href="https://www.utk.edu/visit/" className="nav-link px-0 text-light text-uppercase">Visit</a></small></li>
-                      <li className="col-auto"><small><a href="https://www.utk.edu/apply/" className="nav-link px-0 text-light text-uppercase">Apply</a></small></li>
-                      <li className="col-auto"><small><a href="https://www.utk.edu/give/" className="nav-link px-0 text-light text-uppercase">Give</a></small></li>
+                      <li className="col-auto"><small><a href="/requestinfo/" className="nav-link px-0 text-light text-uppercase text-small text-white btn-request">Request Info</a></small></li>
+                      <li className="col-auto"><small><a href="/visit/" className="nav-link px-0 text-light text-uppercase">Visit</a></small></li>
+                      <li className="col-auto"><small><a href="/apply/" className="nav-link px-0 text-light text-uppercase">Apply</a></small></li>
+                      <li className="col-auto"><small><a href="/give/" className="nav-link px-0 text-light text-uppercase">Give</a></small></li>
                       <li className="col-auto">
                         <button type="button" className="btn btn-search text-uppercase text-light navbar-toggler col-auto collapsed" id="btn-searchclose" data-bs-toggle="modal" data-bs-target="#searchModal" aria-label="Toggle search">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" id="searchHeader-close">
