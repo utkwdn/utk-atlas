8c7e10213d14c8a1b891403d5b6fd76c1058cce0
Sandra Paduch <69162526+spworky@users.noreply.github.com>
2022-04-27 17:18:38 -0400

    feat – Add Vision
    + Vision added as new file in atlas
    + styles are working off static style.css
    + Animations are working

--- src/pages/vision.js
+++ src/pages/vision.js
@@ -1,12 +1,12 @@
 import Layout from "../components/Layout";
 import Head from "next/head";
-
+// import styles from "scss/vision.scss";
 function Vision() {
   return (
     <Layout>
       <Head>
-        <link rel="stylesheet" href="css/vision.css" />
-        <script defer type="text/javascript" src="/js/vision.js"></script>
+        // <link rel="stylesheet" href="css/vision.css" />
+        // <script defer type="text/javascript" src="/js/vision.js"></script>
       </Head>
 
       <section className="hero">
@@ -26,7 +26,6 @@ function Vision() {
             <div className="triangle-bottomright"></div>
           </div>
         </div>
-
         <section className="vision">
           <h2 className="nod">Our Vision</h2>
           <p className="lead">
@@ -122,8 +121,213 @@ function Vision() {
             </ul>
           </div>
         </section>
-        <div className="skinnyLongImg"></div>
       </section>
+      <div className="skinnyLongImg"></div>
+
+      <section className="goalsContainer">
+        <h2 className="nod">
+          <span className="printLineHeader"></span>
+          <span className="bringForward">Our Goals and Objectives</span>
+        </h2>
+
+        <div className="goalsArticleContainer">
+          <article className="goalsObj">
+            <div className="objLeft ">
+              <h3 className="text-uppercase objTitleL01">
+                Cultivating the{" "}
+                <span className="boldPunch">Volunteer Experience</span>
+              </h3>
+              <div className="printLineComplex camo01Container">
+                <div className="printLinePattern torchCamo01"></div>
+              </div>
+              <p className="lead offset">
+                Demonstrate leadership in providing high-quality educational
+                opportunities for people at every stage of life, whenever and
+                wherever they seek to learn
+              </p>
+              <ul className="ObjList">
+                <li>
+                  <span className="textPopLight">Provide</span> learners at all
+                  levels with opportunities to engage in rich learning and in
+                  scholarship that is collaborative, inquiry-based,
+                  experiential, affordable, and relevant
+                </li>
+                <li>
+                  <span className="textPopLight">Deliver</span> educational
+                  opportunities that are responsive to the needs of learners
+                </li>
+                <li>
+                  <span className="textPopLight">Support</span> curricular
+                  innovations that align with 21st-century workforce needs and
+                  our research strengths and priorities
+                </li>
+                <li>
+                  <span className="textPopLight">Ensure</span> that the programs
+                  we offer are accessible to communities across Tennessee and
+                  beyond
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article className="goalsObj greyBkg">
+            <div className="objRight">
+              <h3 className="text-uppercase printLineLeft objTitleContainerR01">
+                <span className="objTitleR01">
+                  Conducting Research{" "}
+                  <span className="boldPunch">
+                    That Makes Life and Lives Better
+                  </span>
+                </span>
+              </h3>
+              <p className="lead offset">
+                Advance the frontiers of knowledge to create a more just,
+                prosperous, and sustainable future through world-class research,
+                scholarship, and creative work
+              </p>
+              <ul className="ObjList">
+                <li>
+                  <span className="textPopLight">Strengthen</span> the impact
+                  and reputation of our research, scholarship, and creative
+                  activities
+                </li>
+                <li>
+                  <span className="textPopLight">Ensure</span> that the research
+                  outcomes we generate lead to the creation of a more just,
+                  prosperous, and sustainable future
+                </li>
+                <li>
+                  <span className="textPopLight">Invest</span> in
+                  state-of-the-art research infrastructure and frameworks to
+                  ensure that our researchers, scholars, and creatives continue
+                  to work at the knowledge frontier
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article className="goalsObj ">
+            <div className="objLeft">
+              <h3 className="text-uppercase objTitleL02">
+                Ensuring a Culture Where{" "}
+                <span className="boldPunch">Vol is a Verb</span>
+              </h3>
+              <div className="printLineComplex camo02Container">
+                <div className="printLinePattern torchCamo02"></div>
+              </div>
+              <p className="lead offset">
+                Develop and sustain a nurturing university culture where
+                diversity and community are enduring sources of strength
+              </p>
+              <ul className="ObjList">
+                <li>
+                  <span className="textPopLight">Implement</span> structures and
+                  practices that attract and retain a diverse community of
+                  faculty, staff, and students and that support a culture where
+                  everyone matters and belongs
+                </li>
+                <li>
+                  <span className="textPopLight">Challenge</span> students to
+                  examine their understandings of the world and their capacity
+                  to act as members of an inclusive community
+                </li>
+                <li>
+                  <span className="textPopLight">Nurture</span> change that
+                  supports inclusive behaviors and a culture of respectful
+                  dialogue to create greater understanding of difference,
+                  starting with our administration, faculty, and staff
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article className="goalsObj greyBkg">
+            <div className="objRight">
+              <h3 className="text-uppercase objTitleContainerR01 printLineLeft">
+                <span className="objTitleR02">
+                  Making Ourselves{" "}
+                  <span className="boldPunch">Nimble and Adaptable</span>
+                </span>
+              </h3>
+              <p className="lead offset">
+                Empower and sustain a culture of collaboration, adaptability,
+                and innovation
+              </p>
+              <ul className="ObjList">
+                <li>
+                  <span className="textPopLight">Enhance</span> a campus-wide
+                  culture of innovation and collaboration at all levels
+                </li>
+                <li>
+                  <span className="textPopLight">Adapt</span> current structures
+                  and systems to foster identity and collaboration while
+                  supporting innovation
+                </li>
+                <li>
+                  <span className="textPopLight">Foster</span> organizational
+                  agility and innovation by developing incentives for innovative
+                  and transformational work that also breaks or blurs the lines
+                  of organizational silos
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article className="goalsObj ">
+            <div className="objLeft">
+              <h3 className="text-uppercase objTitleL03">
+                Embodying the Modern R1,{" "}
+                <span className="boldPunch">Land-Grant University</span>
+              </h3>
+              <div className="printLineComplex camo03Container">
+                <div className="printLinePattern torchCamo03"></div>
+              </div>
+              <p className="lead offset">
+                Connect with every Tennessean and with communities around the
+                world, inspiring future Volunteers to join our diverse community
+              </p>
+              <ul className="ObjList">
+                <li>
+                  <span className="textPopLight">Deepen</span> and extend the
+                  university’s ability to connect with Tennesseans
+                </li>
+                <li>
+                  <span className="textPopLight">Establish</span>, build upon,
+                  and streamline processes to identify community needs and
+                  activate university resources to meet those needs
+                </li>
+              </ul>
+            </div>
+          </article>
+        </div>
+      </section>
+      <section className="explore">
+        <div className="angleContainer02">
+          <div className="angleOutro">
+            <p>
+              We examined the changing landscape of higher education and
+              analyzed the university’s prospects for the future.
+            </p>
+            <p>
+              <a
+                href="https://chancellor.utk.edu/vision/envisioning-the-future-of-ut-knoxville/"
+                className="exploreLink"
+              >
+                <span aria-hidden="true">Explore Our Vision Process</span>{" "}
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="#ff8200"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"></path>
+                  </svg>
+                </span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <div className="skinnyLongImg visionFooter"></div>
     </Layout>
   );
 }
