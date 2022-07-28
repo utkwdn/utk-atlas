5e1a43d3ca33041db160b97d5540661e4153b7e4
Sandra Paduch <69162526+spworky@users.noreply.github.com>
2022-07-28 15:11:32 -0400

    fix – Programs and Intro Component
    Fix breaking errors
     – update programs.json path
     – add comments to academics/programs.tsx about markup
     - fix components/Intro.tsx by adding type definitions for props
     – add comments to components/Intro.tsx about usage and goals

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,12 +1,31 @@
-import styles from "scss/components/Intro.module.scss";
-const Intro = ({ title, theme, intro }) => {
+import styles from 'scss/components/Intro.module.scss';
+
+type IntroProps = {
+  title: string;
+  theme: string;
+  intro: object;
+};
+
+/** Intro component created to be able to easily swap out the main intro chunk of content for a page.
+ * Contains:
+ * – demphasized H1 {title}
+ * – emphasized <p> {theme} (main message/title for the page treated visually like an H1)
+ * – flexible {intro} sub-section that contains supporting graphic/video/etc (currently html added in each instance)
+ * (eventually envision {intro} using other swappable or nested components to add flexibility of content type used in this space – but these need to be identified and created. TBD)
+ */
+
+const Intro: React.FunctionComponent<IntroProps> = ({
+  title,
+  theme,
+  intro,
+}) => {
   return (
-    <section className={styles["intro-container"]}>
-      <div className={styles["page-title-group"]}>
-        <hr className={styles["oa-thick-bar"]} />
-        <h1 className={[styles.area, "text-condensed"].join(" ")}>{title}</h1>
-        <div className={styles["title-container"]}>
-          <p className={styles["title"]}>{theme}</p>
+    <section className={styles['intro-container']}>
+      <div className={styles['page-title-group']}>
+        <hr className={styles['oa-thick-bar']} />
+        <h1 className={[styles.area, 'text-condensed'].join(' ')}>{title}</h1>
+        <div className={styles['title-container']}>
+          <p className={styles['title']}>{theme}</p>
         </div>
       </div>
       <div className={styles.intro}>{intro}</div>
--- src/pages/academics/programs.tsx
+++ src/pages/academics/programs.tsx
@@ -1,39 +1,130 @@
-import Layout from "../../components/Layout";
-import Head from "next/head";
-import Intro from "../../components/Intro";
-import styles from "scss/pages/Programs.module.scss";
-import Script from "next/script";
-import { prependOnceListener } from "process";
-import content from "/src/_data/programs.json";
+import Layout from '../../components/Layout';
+import Head from 'next/head';
+import Intro from '../../components/Intro';
+import styles from 'scss/pages/Programs.module.scss';
+import Script from 'next/script';
+import { prependOnceListener } from 'process';
+import content from '../../_data/programs.json';
 
 function Programs() {
-
-console.log(content)
+  console.log(content);
 
   return (
-
     <Layout>
       <Intro
-        title={"Programs"}
-        theme={"Find Your Passion"}
+        title={'Programs'}
+        theme={'Find Your Passion'}
         intro={
           <p>
-            "So many options and paths to choose from. Find what makes you happy
-            and live a great life."
+            So many options and paths to choose from. Find what makes you happy
+            and live a great life.
           </p>
         }
       />
 
-  
       <section className={styles.areasContainer}>
         {/* CONTENT HOLDER FOR CARDS */}
+
+        {/* Individual Card 01 – 
+         Card design that will be dynamically created for each based on available data of Areasofstudy
+         Potentially 14 cards will eventually show */}
         <div className={styles.areaCard}>
           <div className={styles.areaTeaser}>
             <div className={styles.areaTitle}>
               <h2>
-                Art<span className={styles["oa-period"]}>.</span>
+                CategoryA<span className={styles['oa-period']}>.</span>
                 <br />
-                Performance<span className={styles["oa-period"]}>.</span>
+                CategoryB<span className={styles['oa-period']}>.</span>
+              </h2>
+            </div>
+            {/* End ProgramTitle */}
+            <div className={styles.areaImage}>
+              <img src="//images.utk.edu/designsystem/academics/areas/art-performance.jpg" />
+            </div>
+            {/* End areaImage */}
+            <p>description</p>
+            <a className={[styles.btnTweak, 'btn-link'].join(' ')} href="#">
+              Request Information
+              {}
+            </a>
+          </div>
+          {/* End programTeaser */}
+          <div className={styles.programList}>
+            <ul>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>program</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>degrees</span>
+                  college
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>program</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>degrees</span>
+                  college
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>program</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>degrees</span>
+                  college
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>program</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>degrees</span>
+                  college
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>program</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>degrees</span>
+                  college
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>program</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>degrees</span>
+                  college
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* End of Individual Card 01 */}
+
+        {/* Start of Individual Card 02 – 
+  Eventually all cards past this point will be deleted when data is looped for Card 01 */}
+        <div className={styles.areaCard}>
+          <div className={styles.areaTeaser}>
+            <div className={styles.areaTitle}>
+              <h2>
+                Art<span className={styles['oa-period']}>.</span>
+                <br />
+                Performance<span className={styles['oa-period']}>.</span>
               </h2>
             </div>
             {/* End ProgramTitle */}
@@ -46,7 +137,7 @@ console.log(content)
               aesthetics to change cities or shape the understanding of print
               and digital landscapes.
             </p>
-            <a className={[styles.btnTweak, "btn-link"].join(" ")} href="#">
+            <a className={[styles.btnTweak, 'btn-link'].join(' ')} href="#">
               Request Information
             </a>
           </div>
@@ -55,7 +146,98 @@ console.log(content)
             <ul>
               <li>
                 <a href="#">
-                  <span className={styles.programName}>{content.categoryA}</span>
+                  <span className={styles.programName}>Art History</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>BA, MA</span>
+                  College of Archtiecture and Design
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>Art History</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>BA, MA</span>
+                  College of Archtiecture and Design
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>Art History</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>BA, MA</span>
+                  College of Archtiecture and Design
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>Art History</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>BA, MA</span>
+                  College of Archtiecture and Design
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>Art History</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>BA, MA</span>
+                  College of Archtiecture and Design
+                </span>
+              </li>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>Art History</span>
+                </a>
+                <br />
+                <span className={styles.programCollege}>
+                  <span className={styles.programDegrees}>BA, MA</span>
+                  College of Archtiecture and Design
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* End of Individual Card 02 */}
+        {/* Start of Individual Card 03 */}
+        <div className={styles.areaCard}>
+          <div className={styles.areaTeaser}>
+            <div className={styles.areaTitle}>
+              <h2>
+                Art<span className={styles['oa-period']}>.</span>
+                <br />
+                Performance<span className={styles['oa-period']}>.</span>
+              </h2>
+            </div>
+            {/* End ProgramTitle */}
+            <div className={styles.areaImage}>
+              <img src="//images.utk.edu/designsystem/academics/areas/art-performance.jpg" />
+            </div>
+            {/* End areaImage */}
+            <p>
+              Learn how to shape the world around you through visual arts. Use
+              aesthetics to change cities or shape the understanding of print
+              and digital landscapes.
+            </p>
+            <a className={[styles.btnTweak, 'btn-link'].join(' ')} href="#">
+              Request Information
+            </a>
+          </div>
+          {/* End programTeaser */}
+          <div className={styles.programList}>
+            <ul>
+              <li>
+                <a href="#">
+                  <span className={styles.programName}>Art History</span>
                 </a>
                 <br />
                 <span className={styles.programCollege}>
@@ -117,103 +299,16 @@ console.log(content)
           </div>
         </div>
 
-        <div className={styles.areaCard}>
-          <div className={styles.areaTeaser}>
-            <div className={styles.areaTitle}>
-              <h2>
-                Art<span className={styles["oa-period"]}>.</span>
-                <br />
-                Performance<span className={styles["oa-period"]}>.</span>
-              </h2>
-            </div>
-            {/* End ProgramTitle */}
-            <div className={styles.areaImage}>
-              <img src="//images.utk.edu/designsystem/academics/areas/art-performance.jpg" />
-            </div>
-            {/* End areaImage */}
-            <p>
-              Learn how to shape the world around you through visual arts. Use
-              aesthetics to change cities or shape the understanding of print
-              and digital landscapes.
-            </p>
-            <a className={[styles.btnTweak, "btn-link"].join(" ")} href="#">
-              Request Information
-            </a>
-          </div>
-          {/* End programTeaser */}
-          <div className={styles.programList}>
-            <ul>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
+        {/* End of Individual Card 03 */}
 
+        {/* Start of Individual Card 04 */}
         <div className={styles.areaCard}>
           <div className={styles.areaTeaser}>
             <div className={styles.areaTitle}>
               <h2>
-                Art<span className={styles["oa-period"]}>.</span>
+                Art<span className={styles['oa-period']}>.</span>
                 <br />
-                Performance<span className={styles["oa-period"]}>.</span>
+                Performance<span className={styles['oa-period']}>.</span>
               </h2>
             </div>
             {/* End ProgramTitle */}
@@ -226,7 +321,7 @@ console.log(content)
               aesthetics to change cities or shape the understanding of print
               and digital landscapes.
             </p>
-            <a className={[styles.btnTweak, "btn-link"].join(" ")} href="#">
+            <a className={[styles.btnTweak, 'btn-link'].join(' ')} href="#">
               Request Information
             </a>
           </div>
@@ -296,98 +391,9 @@ console.log(content)
             </ul>
           </div>
         </div>
-
-        <div className={styles.areaCard}>
-          <div className={styles.areaTeaser}>
-            <div className={styles.areaTitle}>
-              <h2>
-                Art<span className={styles["oa-period"]}>.</span>
-                <br />
-                Performance<span className={styles["oa-period"]}>.</span>
-              </h2>
-            </div>
-            {/* End ProgramTitle */}
-            <div className={styles.areaImage}>
-              <img src="//images.utk.edu/designsystem/academics/areas/art-performance.jpg" />
-            </div>
-            {/* End areaImage */}
-            <p>
-              Learn how to shape the world around you through visual arts. Use
-              aesthetics to change cities or shape the understanding of print
-              and digital landscapes.
-            </p>
-            <a className={[styles.btnTweak, "btn-link"].join(" ")} href="#">
-              Request Information
-            </a>
-          </div>
-          {/* End programTeaser */}
-          <div className={styles.programList}>
-            <ul>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-              <li>
-                <a href="#">
-                  <span className={styles.programName}>Art History</span>
-                </a>
-                <br />
-                <span className={styles.programCollege}>
-                  <span className={styles.programDegrees}>BA, MA</span>
-                  College of Archtiecture and Design
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-        {/* End areaContainer */}
+        {/* End of Individual Card 04 */}
       </section>
+      {/* End areasContainer */}
     </Layout>
   );
 }
