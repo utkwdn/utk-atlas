471e70ef3c9b9c18a19580999cc92fa624e3a76d
Sandra Paduch <69162526+spworky@users.noreply.github.com>
2022-08-19 16:57:22 -0400

    add accordion to storybook

--- src/stories/blocks/Accordion.stories.tsx
+++ src/stories/blocks/Accordion.stories.tsx
@@ -13,4 +13,20 @@ export default {
       </div>
     ),
   ],
+} as ComponentMeta<typeof Accordion>;
+
+const Template: ComponentStory<typeof Accordion> = (args) => (
+  <Accordion {...args} />
+);
+
+export const Collapsed = Template.bind({});
+Collapsed.args = {
+  attributes: {
+    accordionID: 'id',
+  },
+};
+
+export const Expanded = Template.bind({});
+Expanded.args = {
+  attributes: {},
 };
