201fee2aec3fd18b810cd61b3ae2989d965ddbe1
Michael Warner <michaelwarner@Michaels-MacBook-Pro.local>
2022-08-19 12:57:08 -0400

    blocks working with each other and in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,19 @@
+import '../src/scss/main.scss';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  layout: 'fullscreen',
+  options: {
+    storySort: {
+      method: 'alphabetical',
+      includeNames: true,
+      order: ['Atoms', 'Molecules', 'Organisms', 'Page Demos'],
+    },
+  },
+};
