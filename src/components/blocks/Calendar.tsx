d979de71213360c75d852100598c464cb46aaba5
Sandra Paduch <69162526+spworky@users.noreply.github.com>
2022-09-02 15:41:13 -0400

    refactor to useEffect – still not working. Think there are some attributes that might need to be added to plugin side

--- src/components/blocks/Calendar.tsx
+++ src/components/blocks/Calendar.tsx
@@ -1,67 +1,63 @@
 import { AttributesBlock } from 'types/AttributesBlock';
 import { UtkwdsCalendarBlockAttributes } from 'client';
-// import React, { useEffect } from 'react';
-import Script from 'next/script';
+import React, { useEffect } from 'react';
+import Test from './Test';
 
 interface Props {
   attributes: Partial<UtkwdsCalendarBlockAttributes>;
   innerBlocks?: AttributesBlock[];
+  
 }
 
 const Calendar = ({
-//   attributes: {
-//     className,
-//     widgetType,
-//     place,
-//     department,
-//     group,
-//     all_types,
-//     daysAhead,
-//     numResults,
-//     keywords,
-//     featuredS,
-//     sponsoredS,
-//     matchingS,
-//     pastS,
-//     hideDescS,
-//     truncateS,
-//     htmlDescS,
-//     evImageS,
-//     evTimeS,
-//     viewAllS,
-//     newWinS,
-//     hideDropS,
-//     mustMatch,
-//     exAll_types,
-//     incStyleS,
-//     template,
-//   },
-// }: Props) => (
-//   <>
-//     <Script
-//       src={`'https://calendar.utk.edu/widget/'${
-//         widgetType || ''
-//       }'?schools=utk&venues='${place || ''}'&departments='${
-//         department || ''
-//       }'&groups='${group || ''}'&types='${all_types}'&days='${
-//         daysAhead || ''
-//       }'&num='${numResults || ''}'&tags='${keywords || ''}${featuredS || ''}${
-//         sponsoredS || ''
-//       }${matchingS || ''}${pastS || ''}${hideDescS || ''}${truncateS || ''}${
-//         htmlDescS || ''
-//       }${evImageS || ''}${evTimeS || ''}${viewAllS || ''}${newWinS || ''}${
-//         hideDropS || ''
-//       }'&match='${
-//         mustMatch || ''
-//       }'&exclude_types='${exAll_types}'&container=localist-widget-12345'${
-//         incStyleS || ''
-//       }'&template='${template || ''}`}
-//       strategy="lazyOnload"
-//     />
-//     <div className={className || ''}>
-//       <div id={'localist-widget-12345'} className={'localist-widget'}></div>
-//     </div>
-//   </>
-);
+  attributes: {
+    className,
+    widgetType,
+    place,
+    department,
+    group,
+    daysAhead,
+    numResults,
+    keywords,
+    featuredS,
+    sponsoredS,
+    matchingS,
+    pastS,
+    hideDescS,
+    truncateS,
+    htmlDescS,
+    evImageS,
+    evTimeS,
+    viewAllS,
+    newWinS,
+    hideDropS,
+    mustMatch,
+    incStyleS,
+    template,
+    topic,
+    audience,
+    exType,
+    exTopic,
+    exAudience,
+  }}: Props) => 
+ (
+
+  // Do the need to be added as attributes in the plugin? – like slug from alerts?
+  // const all_types = {`${widgetType || ''}','${topic || ''}','${audience || ''}`};
+  // const exAll_types = {`${exType}','${exTopic}','${exAudience}`};
+
+   useEffect(() => {
+    const calendarUrl = document.createElement('script');
+    calendarUrl.src = {`'https://calendar.utk.edu/widget/' ${widgetType ||''}'?schools=utk&venues='${place ||''}'&departments='${department ||''}'&groups='${group ||''}'&types='${all_types}'&days='${daysAhead ||''}'&num='${numResults ||''}'&tags='${keywords ||''}${featuredS ||''}${sponsoredS ||''}${matchingS ||''}${pastS ||''}${hideDescS ||''}${truncateS ||''}${htmlDescS ||''}${evImageS ||''}${evTimeS ||''}${viewAllS ||''}${newWinS ||''}${hideDropS ||''}'&match='${mustMatch ||''}'&exclude_types='${exAll_types}'&container=localist-widget-12345'${incStyleS ||''}'&template='${template ||''}`};
+
+    document.body.appendChild(calendarUrl);
+  });
+
+  <div className={className || ''}>
+  <div id={'localist-widget-12345'} className={'localist-widget'}>
+    
+  </div>
+</div>
+  );
 
 export default Calendar;
