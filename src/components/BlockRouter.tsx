ae16e72afef38c8b51410f9d50fab5df3e59307c
Sandra Paduch <69162526+spworky@users.noreply.github.com>
2022-09-02 16:33:44 -0400

    feat – add CardBody: component, router, story

--- src/components/BlockRouter.tsx
+++ src/components/BlockRouter.tsx
@@ -3,6 +3,7 @@ import {
   UtksdsAccordionBlockAttributes,
   UtkwdsAccordionFoldBlockAttributes,
   UtkwdsAlertBlockAttributes,
+  UtkwdsCardBodyBlockAttributes,
   UtkwdsHorizontalRuleBlockAttributes,
   UtkwdsLeadBlockAttributes,
 } from 'client';
@@ -12,6 +13,7 @@ import HorizontalRule from './blocks/HorizontalRule';
 import Accordion from './blocks/Accordion';
 import AccordionFold from './blocks/AccordionFold';
 import Alert from './blocks/Alert';
+import CardBody from './blocks/CardBody';
 import Lead from './blocks/Lead';
 
 interface Props {
@@ -63,6 +65,15 @@ const BlockRouter = ({ block }: Props) => {
 
     case 'utkwds/alert': {
       return <Alert attributes={attributes as UtkwdsAlertBlockAttributes} />;
+    }
+
+    case 'utkwds/card-body': {
+      return (
+        <CardBody
+          innerBlocks={innerBlocks || []}
+          attributes={attributes as UtkwdsCardBodyBlockAttributes}
+        />
+      );
     }
 
     case 'utkwds/lead': {
---
+++ src/components/blocks/CardBody.tsx
@@ -0,0 +1,20 @@
+import { AttributesBlock } from 'types/AttributesBlock';
+import { UtkwdsCardBodyBlockAttributes } from 'client';
+import BlockRouter from 'components/BlockRouter';
+
+interface Props {
+  attributes: Partial<UtkwdsCardBodyBlockAttributes>;
+  innerBlocks?: AttributesBlock[];
+}
+
+const CardBody = ({
+  attributes: { className, textColor },
+  innerBlocks,
+}: Props) => (
+  <div className={`'card-body' ${className || ''} ${textColor || ''}`}>
+    {!!innerBlocks?.length &&
+      innerBlocks.map((block, i) => <BlockRouter block={block} key={i} />)}
+  </div>
+);
+
+export default CardBody;
