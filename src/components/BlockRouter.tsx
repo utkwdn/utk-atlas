771ac816e452c764a062225e994bb8397a844150
Sandra Paduch <69162526+spworky@users.noreply.github.com>
2022-09-02 16:51:53 -0400

    feat – Add CardFooter: component, router, story

--- src/components/BlockRouter.tsx
+++ src/components/BlockRouter.tsx
@@ -3,6 +3,7 @@ import {
   UtksdsAccordionBlockAttributes,
   UtkwdsAccordionFoldBlockAttributes,
   UtkwdsAlertBlockAttributes,
+  UtkwdsCardFooterBlockAttributes,
   UtkwdsHorizontalRuleBlockAttributes,
   UtkwdsLeadBlockAttributes,
 } from 'client';
@@ -12,6 +13,7 @@ import HorizontalRule from './blocks/HorizontalRule';
 import Accordion from './blocks/Accordion';
 import AccordionFold from './blocks/AccordionFold';
 import Alert from './blocks/Alert';
+import CardFooter from './blocks/CardFooter';
 import Lead from './blocks/Lead';
 
 interface Props {
@@ -63,6 +65,14 @@ const BlockRouter = ({ block }: Props) => {
 
     case 'utkwds/alert': {
       return <Alert attributes={attributes as UtkwdsAlertBlockAttributes} />;
+    }
+
+    case 'utkwds/card-footer': {
+      return (
+        <CardFooter
+          attributes={attributes as UtkwdsCardFooterBlockAttributes}
+        />
+      );
     }
 
     case 'utkwds/lead': {
---
+++ src/stories/blocks/CardFooter.stories.tsx
@@ -0,0 +1,29 @@
+import CardFooter from 'components/blocks/CardFooter';
+
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'Blocks/Molecules/CardFooter',
+  component: CardFooter,
+  decorators: [
+    (Story) => (
+      <div className="container">
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof CardFooter>;
+
+const Template: ComponentStory<typeof CardFooter> = (args) => (
+  <CardFooter {...args} />
+);
+
+export const NotMuted = Template.bind({});
+NotMuted.args = {
+  attributes: {
+    content: 'This is a Card Footer',
+    muted: false,
+    mutedClass: '',
+  },
+};
