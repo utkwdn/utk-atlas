8cfd71aaa3bea8f3f270b827a95c561da56abb5c
Purdy, Michael Shawn <purdy@utk.edu>
2022-12-09 16:34:41 -0500

    Adding alpha and maps back to the universal search modal. Cosmetic changes

--- src/components/SiteSearch.tsx
+++ src/components/SiteSearch.tsx
@@ -120,19 +120,19 @@ const SiteSearch = () => {
             name="q"
             id="q"
           />
-          <button type="submit" className="btn btn-secondary">
+          <button type="submit" className="btn btn-utlink">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
-              fill="currentColor"
+              fill="white"
               className="bi bi-search"
               aria-hidden="true"
               viewBox="0 0 16 16"
             >
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>{' '}
-            <span className="visually-hidden">Search</span>
+            <span className="text-white">Search</span>
           </button>
         </div>
       </form>
