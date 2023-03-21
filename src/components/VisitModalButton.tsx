d762c46f6c9b30d2ab78daeecc6dde6a7ecd9d0e
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-7.local>
2023-03-21 11:48:43 -0400

    refactor(visitModalButton): preload forms
    preload forms for better performance and add small style tweaks

--- src/components/VisitModalButton.tsx
+++ src/components/VisitModalButton.tsx
@@ -1,18 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import SlateFormEmbed from 'components/SlateFormEmbed';
-import styles from 'scss/components/PageTitle.module.scss';
+import styles from 'scss/components/ModalButton.module.scss';
 
 function VisitModalButton() {
+  const [isHydrated, setIsHydrated] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [chosenFormId, setChosenFormId] = useState('');
-  const [chosenFormUrl, setChosenFormUrl] = useState('');
+  const [showUndergradForm, setShowUndergradForm] = useState(false);
+  const [showGradForm, setShowGradForm] = useState(false);
+  const [showButtons, setShowButtons] = useState(true);
+
+  const UNDERGRAD_ID = 'e20f8349-6ef3-4b8c-b48d-f25ef6174c8d';
+  const GRAD_ID = '5880822a-64f0-4764-bca2-5f401da0fb37';
 
   const handleCloseModal = () => {
     setShowModal(false);
     setTimeout(() => {
-      setChosenFormId('');
-      setChosenFormUrl('');
+      setShowUndergradForm(false);
+      setShowGradForm(false);
+      setShowButtons(true);
     }, 500);
   };
   const handleShowModal = (e: any) => {
@@ -23,15 +29,20 @@ function VisitModalButton() {
   const handleFormChoice = (e: any, formChoice: string) => {
     e.preventDefault();
 
-    const UNDERGRAD_ID = 'e20f8349-6ef3-4b8c-b48d-f25ef6174c8d';
-    const GRAD_ID = '5880822a-64f0-4764-bca2-5f401da0fb37';
-    const UNDERGRAD_URL = `govols.utk.edu`;
-    const GRAD_URL = `apply.gradschool.utk.edu`;
-
-    setChosenFormUrl(formChoice === 'undergrad' ? UNDERGRAD_URL : GRAD_URL);
-    setChosenFormId(formChoice === 'undergrad' ? UNDERGRAD_ID : GRAD_ID);
+    if (formChoice === 'undergrad') {
+      setShowUndergradForm(true);
+    }
+    if (formChoice === 'grad') {
+      setShowGradForm(true);
+    }
+    setShowButtons(false);
   };
-  return (
+
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+
+  return isHydrated ? (
     <>
       <div className="is-layout-flex wp-block-buttons">
         <div className="wp-block-button is-style-outline">
@@ -45,67 +56,70 @@ function VisitModalButton() {
         </div>
       </div>
       <Modal
-        size={chosenFormId === '' ? 'lg' : 'xl'}
+        size={showButtons ? 'lg' : 'xl'}
         show={showModal}
         onHide={handleCloseModal}
         centered={true}
       >
         <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
-          {chosenFormId !== '' ? (
-            <div>
-              <SlateFormEmbed
-                id={chosenFormId}
-                scriptSrc={
-                  `https://${chosenFormUrl}/register/?id=${chosenFormId}&output=embed&div=form_${chosenFormId}` +
-                  (location.search.length > 1
-                    ? '&' + location.search.substring(1)
-                    : '')
-                }
-              />
-            </div>
-          ) : (
+          <div style={{ display: showUndergradForm ? 'block' : 'none' }}>
+            <SlateFormEmbed
+              id={UNDERGRAD_ID}
+              scriptSrc={
+                `https://govols.utk.edu/register/?id=${UNDERGRAD_ID}&output=embed&div=form_${UNDERGRAD_ID}` +
+                (location.search.length > 1
+                  ? '&' + location.search.substring(1)
+                  : '')
+              }
+            />
+          </div>
+          <div style={{ display: showGradForm ? 'block' : 'none' }}>
+            <SlateFormEmbed
+              id={GRAD_ID}
+              scriptSrc={
+                `https://apply.gradschool.utk.edu/register/?id=${GRAD_ID}&output=embed&div=form_${GRAD_ID}` +
+                (location.search.length > 1
+                  ? '&' + location.search.substring(1)
+                  : '')
+              }
+            />
+          </div>
+          <div
+            style={{
+              display: showButtons ? 'flex' : 'none',
+            }}
+            className={styles['formChoiceButtons']}
+          >
+            <h4>GET INFORMATION ON VISITING UT FOR:</h4>
             <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                textAlign: 'center',
-                paddingBottom: '40px',
-              }}
+              className={`is-layout-flex wp-block-buttons ${styles['modalButtons']}`}
             >
-              <h4>GET INFORMATION ON VISITING UT FOR:</h4>
-              <div
-                className="is-layout-flex wp-block-buttons"
-                style={{ marginBottom: '20px', marginTop: '10px' }}
-              >
-                <div className="wp-block-button is-style-outline">
-                  <a
-                    className="wp-block-button__link wp-element-button"
-                    href="#"
-                    onClick={(e) => handleFormChoice(e, 'undergrad')}
-                  >
-                    Incoming undergraduate or transfers
-                  </a>
-                </div>
+              <div className="wp-block-button is-style-outline">
+                <a
+                  className="wp-block-button__link wp-element-button"
+                  href="#"
+                  onClick={(e) => handleFormChoice(e, 'undergrad')}
+                >
+                  Incoming undergraduate or transfers
+                </a>
               </div>
-              <div className="is-layout-flex wp-block-buttons">
-                <div className="wp-block-button is-style-outline">
-                  <a
-                    className="wp-block-button__link wp-element-button"
-                    href="#"
-                    onClick={(e) => handleFormChoice(e, 'grad')}
-                  >
-                    Incoming graduate students
-                  </a>
-                </div>
+              <div className="wp-block-button is-style-outline">
+                <a
+                  className="wp-block-button__link wp-element-button"
+                  href="#"
+                  onClick={(e) => handleFormChoice(e, 'grad')}
+                >
+                  Incoming graduate students
+                </a>
               </div>
             </div>
-          )}
+          </div>
         </Modal.Body>
       </Modal>
     </>
+  ) : (
+    <></>
   );
 }
 
