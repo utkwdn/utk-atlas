c0d78c297c2bccd158a9fefab71fd2441d2f2df2
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-7.local>
2023-10-27 10:58:08 -0400

    Revert "style(fonts): replace Gotham with Montserrat and Sofia"
    This reverts commit 5f5565215673e32c1511cb7e66484e726ef23e0c.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,29 +4,12 @@ import React from 'react';
 import 'scss/main.scss';
 import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
-import { Montserrat, Sofia_Sans_Extra_Condensed } from 'next/font/google';
-
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
-});
-
-const sofia = Sofia_Sans_Extra_Condensed({
-  subsets: ['latin'],
-  display: 'swap',
-});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
     <>
-      <style jsx global>{`
-        :root {
-          --font-montserrat: ${montserrat.style.fontFamily};
-          --font-sofia: ${sofia.style.fontFamily};
-        }
-      `}</style>
       {/* eslint-disable @typescript-eslint/no-unsafe-assignment  */}
       <FaustProvider pageProps={pageProps}>
         <Component {...pageProps} key={router.asPath} />
