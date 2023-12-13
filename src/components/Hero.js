2f7fb022e7888374fd08168e75674c1e4460b3a1
Sandra Paduch <69162526+spworky@users.noreply.github.com>
2023-12-13 15:51:16 -0500

    remove confirm enrollment piece. Commented out for luanch on monday: Monday 12/18 Launch Piece

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -387,8 +387,8 @@ const Hero = () => {
                 {/* End of Q4 November 1st Push */}
               </>
             )}
-
-            <h3 className="subCta">Admitted to UT?</h3>
+            {/* Monday 12/18 Launch Piece */}
+            {/* <h3 className="subCta">Admitted to UT?</h3>
             <a
               href="https://admissions.utk.edu/confirm/"
               className="subLink  pointer-event-visible"
@@ -396,8 +396,10 @@ const Hero = () => {
               // className="subLink text-decoration-none link-underline  pointer-event-visible"
             >
               Confirm enrollment now
-            </a>
+            </a> */}
+            {/* End Monday 12/18 Launch Piece */}
           </div>
+
           {/* End CTA */}
 
           <div className="heroRainHolderTripleA layoutA ">
