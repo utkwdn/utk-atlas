4e2e1f39e0e510b57ab2b81cc2b1a2c7a4251f56
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2024-01-11 10:45:33 -0500

    feature(programs): add ui filtering by aos, college or degree type

--- src/pages/academics/programs/[[...slug]].tsx
+++ src/pages/academics/programs/[[...slug]].tsx
@@ -20,7 +20,7 @@ interface Program {
   college: string
   'area-of-study': string
   degrees: string
-  online: boolean
+  'degree-types': string
   programLink: string
 }
 
@@ -33,7 +33,7 @@ function Programs() {
         college: '',
         'area-of-study': '',
         degrees: '',
-        online: false,
+        'degree-types': '',
         programLink: '#'
       }
     ]
@@ -54,21 +54,29 @@ function Programs() {
     return 0;
   }
 
-  const router = useRouter();  
+  // const router = useRouter();  
 
 
-  if(router.query.slug && router.query.slug.length > 1) {
-    const newFilterType = router.query.slug[0];
-    const newFilterTerm = router.query.slug[1].replaceAll('-',' ');
+  // if(router.query.slug && router.query.slug.length > 1) {
+  //   const newFilterType = router.query.slug[0];
+  //   const newFilterTerm = router.query.slug[1];
 
-    console.log(newFilterTerm)
+  //   if(filterType !== newFilterType) {
+  //     setFilterType(newFilterType);
+  //   }
+    
+  //   if(filterTerm !== newFilterTerm) {
+  //     setFilterTerm(newFilterTerm);
+  //   }
+  // }
 
-    if(filterType !== newFilterType) {
-      setFilterType(newFilterType);
+  const handleFilterChange = (chosenFilterType: string, value: string) => {
+    if(filterType !== chosenFilterType) {
+      setFilterType(chosenFilterType);
     }
     
-    if(filterTerm !== newFilterTerm) {
-      setFilterTerm(newFilterTerm);
+    if(filterTerm !== value) {
+      setFilterTerm(value);
     }
   }
 
@@ -77,7 +85,7 @@ function Programs() {
     if(programs) {
       const flatPrograms = programs?.map((program) => {
         const degreeString = program.degrees?.nodes?.map(function (e) {return e.name;}).join(', ') || '';
-        const hasOnline = degreeString?.includes("Online") ? true : false;
+        const degreeType = program.degrees?.nodes?.map(function (e) {return e.description;}).join(', ') || '';
 
         return (
           {
@@ -86,25 +94,38 @@ function Programs() {
             college: program.colleges?.nodes[0].name || '',
             'area-of-study': program.areasOfStudy?.nodes[0].name || '',
             degrees: degreeString,
-            online: hasOnline,
+            'degree-types': degreeType,
             programLink: program.programDetailsFields?.url || '#'
           }
         )
       }).sort(alphabetizeByMajor);
 
       if(filterType !== '' && filterTerm !== '') {
-        const results = matchSorter(flatPrograms, filterTerm, {
-          keys: [filterType],
-          threshold: matchSorter.rankings.ACRONYM,
-        });
-              
-        setActiveItems(results);
+        if(filterType === 'degree-types') {
+          
+          const results = matchSorter(flatPrograms, filterTerm, {
+            keys: [filterType],
+            threshold: matchSorter.rankings.WORD_STARTS_WITH,
+          });
+                
+          setActiveItems(results);
+          
+        } else {
+          const noHyphenTerm = filterTerm.replaceAll('-',' ');
+          const results = matchSorter(flatPrograms, noHyphenTerm, {
+            keys: [filterType],
+            threshold: matchSorter.rankings.ACRONYM,
+          });
+                
+          setActiveItems(results);
+        }
+        
       } else {
         setActiveItems(flatPrograms);
       }
      
     }
-  }, [programs]);
+  }, [programs, filterTerm]);
   
 
   return (
@@ -113,6 +134,60 @@ function Programs() {
         <title>Programs</title>
       </Head>
       <PageTitle title={'Programs'} />
+
+      <section  className={styles.areasContainer}>
+        <div>
+          <p><strong>Area of Study</strong></p>
+          <select name="area-of-study" id="area-of-study" value={filterType === 'area-of-study' ? filterTerm : ''} onChange={e => handleFilterChange('area-of-study', e.target.value)}>
+            <option value="">All Areas of Study</option>
+            <option value="agriculture-and-natural-resources">Agriculture and Natural Resources</option>
+            <option value="architecture-and-design">Architecture and Design</option>
+            <option value="art-and-performance">Art and Performance</option>
+            <option value="business">Business</option>
+            <option value="communication-and-information-sciences">Communication and Information Sciences</option>
+            <option value="education">Education</option>
+            <option value="engineering-math-and-computers">Engineering, Math and Computers</option>
+            <option value="english-and-literature">English and Literature</option>
+            <option value="health-wellness-and-human-sciences">Health, Wellness and Human Sciences</option>
+            <option value="languages-cultures-and-humanities">Languages, Cultures and Humanities</option>
+            <option value="law-and-justice">Law and Justice</option>
+            <option value="science">Science</option>
+            <option value="social-science-and-social-work">Social Science and Social Work</option>
+            <option value="veterinary">Veterinary</option>
+          </select>
+        </div>
+        <div>
+          <p><strong>College</strong></p>
+          <select name="area-of-study" id="area-of-study" value={filterType === 'college' ? filterTerm : ''} onChange={e => handleFilterChange('college', e.target.value)}>
+            <option value="">All Colleges</option>
+            <option value="herbert-college-of-agriculture">Herbert College of Agriculture</option>
+            <option value="architecture-and-design">Architecture and Design</option>
+            <option value="arts-and-sciences">Arts and Sciences</option>
+            <option value="baker-school-of-public-policy-and-public-affairs">Baker School of Public Policy and Public Affairs</option>
+            <option value="haslam-college-of-business">Haslam College of Business</option>
+            <option value="communication-and-information">Communication and Information</option>
+            <option value="education-health-and-human-sciences">Education, Health and Human Sciences</option>
+            <option value="emerging-and-collaborative-studies">Emerging and Collaborative Studies</option>
+            <option value="tickle-college-of-engineering">Tickle College of Engineering</option>
+            <option value="law">Law</option>
+            <option value="music">Music</option>
+            <option value="nursing">Nursing</option>
+            <option value="social-work">Social Work</option>
+            <option value="veterinary-medicine">Veterinary Medicine</option>
+
+
+          </select>
+        </div>
+        <div>
+          <p><strong>Degree Type</strong></p>
+          <select name="area-of-study" id="area-of-study" value={filterType === 'degree-types' ? filterTerm : ''} onChange={e => handleFilterChange('degree-types', e.target.value)}>
+            <option value="">All Degree Types</option>
+            <option value="undergraduate">Undergraduate</option>
+            <option value="graduate">Graduate</option>
+            <option value="online">Online</option>
+          </select>
+        </div>
+      </section>
 
       <section className={styles.areasContainer}>
         {activeItems.length > 0 ? (
@@ -137,7 +212,7 @@ function Programs() {
                       <td style={{padding: '5px 10px'}}>{this_program.college}</td>
                       <td style={{padding: '5px 10px'}}>{this_program['area-of-study']}</td>
                       <td style={{padding: '5px 10px'}}>{this_program.degrees}</td>
-                      {this_program.online
+                      {this_program['degree-types'].includes('Online')
                         ? <td style={{fontSize: 'xx-large', padding: '5px 10px'}}>&#9989;</td>
                         : <td style={{padding: '5px 10px'}}></td>
                       }
@@ -162,36 +237,37 @@ function Programs() {
 export default Programs;
 
 Programs.query = gql(`
-  query GetPrograms {
-    programs(first: 1000, where: {orderby: {field: TITLE, order: ASC}}) {
-      nodes {
-        title
-        colleges {
-          nodes {
-            name
-          }
+query GetPrograms {
+  programs(first: 1000, where: {orderby: {field: TITLE, order: ASC}}) {
+    nodes {
+      title
+      colleges {
+        nodes {
+          name
         }
-        areasOfStudy {
-          nodes {
-            name
-          }
+      }
+      areasOfStudy {
+        nodes {
+          name
         }
-        degrees {
-          nodes {
-            name
-          }
+      }
+      degrees {
+        nodes {
+          name
+          description
         }
-        majors {
-          nodes {
-            name
-          }
+      }
+      majors {
+        nodes {
+          name
         }
-        programDetailsFields {
-          url
-        }
+      }
+      programDetailsFields {
+        url
       }
     }
   }
+}
 `);
 
 export function getStaticProps(ctx: GetStaticPropsContext) {
