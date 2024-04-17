aeefcd4bba39a36f475ec43d29567835e41abd65
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2024-04-17 09:55:16 -0400

    chore(page template): remove comment

--- src/wp-templates/page.tsx
+++ src/wp-templates/page.tsx
@@ -44,7 +44,6 @@ const Page: FaustTemplate<GetPageQuery> = (props) => {
   };
 
   useEffect(() => {
-    // console.log('non-working version');
     // Check if url param 'dmc' is set and save to dynamicSrc if so
     const searchParams = new URLSearchParams(document.location.search);
     const srcParam = searchParams.get('dmc');
