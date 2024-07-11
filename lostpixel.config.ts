44b7867005b6f561e17eb2eedfeb7e90baa536dd
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2024-07-11 10:20:29 -0400

    refactor(test.yml): update config pageshots.pageUrl to pageshots.baseUrl

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -5,7 +5,7 @@ import { CustomProjectConfig } from 'lost-pixel';
 export const config: CustomProjectConfig = {
   pageShots: {
     pages: [{ path: '/app', name: 'app' }],
-    pageUrl: 'http://172.17.0.1:3000',
+    baseUrl: 'http://172.17.0.1:3000',
   },
   generateOnly: true,
   failOnDifference: true,
