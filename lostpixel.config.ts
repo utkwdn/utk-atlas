135022040fba13d9f998234919ced026aa560b70
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2024-07-11 14:23:01 -0400

    refactor(visual regression): refresh from github documentation

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,12 +1,16 @@
-// @ts-nocheck
-
 import { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
   pageShots: {
-    pages: [{ path: '/', name: 'home' }],
-    baseUrl: 'https://hxljyarhr1dyv4e035bh75d33.js.wpenginepowered.com/',
+    pages: [{ path: '/app', name: 'app' }],
+    // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
+    baseUrl: 'http://172.17.0.1:3000',
   },
+  // OSS mode
   generateOnly: true,
   failOnDifference: true,
+
+  // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
+  // lostPixelProjectId: "xxxx",
+  // process.env.LOST_PIXEL_API_KEY,
 };
