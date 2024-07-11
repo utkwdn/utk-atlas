1b33bccd24b8339afe2abc63c3c3a70f07d51d49
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2024-07-11 14:57:29 -0400

    refactor(vrt): update pages in config

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,7 +2,7 @@ import { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
   pageShots: {
-    pages: [{ path: '/', name: 'home' }],
+    pages: [{ path: '/app', name: 'app' }],
     // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
     baseUrl: 'http://172.17.0.1:3000',
   },
