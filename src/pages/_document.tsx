7fd55b7d0a75ea3d01479391a1345bcf64f26e67
Mark Naylor <marknaylor@utkmachinesmbp9.lan>
2024-10-28 12:38:10 -0500

    refactor(hero): update why apply text, add GTM scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,18 @@ class MyDocument extends Document {
           />
 
           {/* Google Tag Manager */}
+          <Script id="google-tag-manager2" strategy="beforeInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NV22T7BX');
+            `}
+          </Script>
+          {/* End Google Tag Manager */}
+
+          {/* Google Tag Manager */}
           <Script id="google-tag-manager" strategy="beforeInteractive">
             {`
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -51,6 +63,14 @@ class MyDocument extends Document {
           /> */}
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NV22T7BX" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
+
           {/* Google Tag Manager (noscript) */}
           <noscript
             dangerouslySetInnerHTML={{
